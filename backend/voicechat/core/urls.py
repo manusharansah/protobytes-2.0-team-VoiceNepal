34e97f43954ac58c4971bcd4739f6d49da11add3
aayamtiwari123 <aayamtiwari2003@gmail.com>
2026-02-14 09:00:05 +0545

    clean(backend): clean backend code

--- backend/voicechat/core/urls.py
+++ backend/voicechat/core/urls.py
@@ -1,14 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     path('speak/', views.speak_text, name='speak_text'),
-
-#     path('api/chat/', views.chat_api, name='chat_api'),
-# ]
-
-
 from django.urls import path
 from . import views
 
